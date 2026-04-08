9363e98bcde5afac74ef9a8e9e29f51d92270c88
Testin33 <azconajuan30@gmail.com>
2026-04-08 18:01:00 +0800

    build:add docs for github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Personal-Portfolio/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+  },
   server: {
     open: 'chrome',
   },
